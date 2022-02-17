afbe0ccd2a7c396b6795f7030c7fd74bf17754f2
darealstyl <jonahball5@hotmail.com>
2022-02-16 19:27:15 -0500

    Started React.js
    began the process of setting up react and bootstrap

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,18 @@
 import { Container } from 'react-bootstrap'
+
+
 import Header from './components/Header'
 import Footer from './components/Footer'
-import HomeScreen from './screens/HomeScreen'
 
 
 function App() {
   return (
     <div>
-      <Header />
+      <Header/>
       <main className="py-3">
-        <h1>
-          <Container>
-            <HomeScreen />
-          </Container>
-          </h1>
+      <Container><h1>Welcome</h1></Container> 
       </main>
-      
-      <Footer />
+      <Footer/>
     </div>
   );
 }
